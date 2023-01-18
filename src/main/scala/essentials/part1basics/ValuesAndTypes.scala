fe9ae71e53a938fb63246e23f6bb8d9bd5d35897
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2023-01-18 17:04:44 -0300

    chore(part1): basic types

---
+++ src/main/scala/essentials/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,23 @@
+package essentials.part1basics
+
+object ValuesAndTypes {
+
+  // values
+  val meaningOfLife: Int = 42
+
+  // type inference
+  val anInteger = 42
+
+  // common types
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = 78 // 4 bytes
+  val aShort: Short = 5263 // 2 bytes
+  val aLong: Long = 52638L // 8 bytes
+  val aFloat: Float = 2.4f // 4 bytes
+  val aDouble: Double = 3.14 // 8 bytes
+  val aString: String = "Scala"
+
+  def main(args: Array[String]): Unit = {}
+
+}
