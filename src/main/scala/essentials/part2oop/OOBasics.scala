14a41420a2a70e7beb25e7f59a199b51dd3abf82
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2023-01-20 19:09:59 -0300

    refactor: define all classes inside the object to avoid conflict

--- src/main/scala/essentials/part2oop/OOBasics.scala
+++ src/main/scala/essentials/part2oop/OOBasics.scala
@@ -2,36 +2,37 @@ package essentials.part2oop
 
 import java.time.LocalDate
 
-// classes
-class Person(val name: String, age: Int): // constructor signature
-  // aux constructor
-  def this(name: String) = this(name, 0)
-  // fields
-  val allCaps = name.toUpperCase
-  // methods
-  def greet(name: String): Unit =
-    println(s"${this.name} says: Hi $name")
-  def greet(): String =
-    s"Hi, everyone, my name is $name"
-
-class Writer(firstName: String, lastName: String, val yearOfBirth: Int):
-  def fullName: String = s"$firstName $lastName"
-
-class Novel(title: String, yearOfRelease: Int, author: Writer):
-  def authorAge: Int = yearOfRelease - author.yearOfBirth
-  // FIXME: create a equals and hash methods on Writer
-  def isWrittenBy(author: Writer): Boolean = this.author == author
-  def copy(newYear: Int): Novel = new Novel(title, newYear, author)
-
-// Immutable counter
-class Counter(count: Int = 0):
-  def increment: Counter = increment(1)
-  def increment(n: Int): Counter = new Counter(count + n)
-  def decrement: Counter = decrement(1)
-  def decrement(n: Int): Counter = increment(-n)
-  def print: Unit = println(count)
-
 object OOBasics extends App:
+
+  // classes
+  class Person(val name: String, age: Int): // constructor signature
+    // aux constructor
+    def this(name: String) = this(name, 0)
+    // fields
+    val allCaps = name.toUpperCase
+    // methods
+    def greet(name: String): Unit =
+      println(s"${this.name} says: Hi $name")
+    def greet(): String =
+      s"Hi, everyone, my name is $name"
+
+  class Writer(firstName: String, lastName: String, val yearOfBirth: Int):
+    def fullName: String = s"$firstName $lastName"
+
+  class Novel(title: String, yearOfRelease: Int, author: Writer):
+    def authorAge: Int = yearOfRelease - author.yearOfBirth
+    // FIXME: create a equals and hash methods on Writer
+    def isWrittenBy(author: Writer): Boolean = this.author == author
+    def copy(newYear: Int): Novel = new Novel(title, newYear, author)
+
+  // Immutable counter
+  class Counter(count: Int = 0):
+    def increment: Counter = increment(1)
+    def increment(n: Int): Counter = new Counter(count + n)
+    def decrement: Counter = decrement(1)
+    def decrement(n: Int): Counter = increment(-n)
+    def print: Unit = println(count)
+
   println("== PERSON ==")
   val aPerson = new Person("John", 26)
   println(aPerson.name)
