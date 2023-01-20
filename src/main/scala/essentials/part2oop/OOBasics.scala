46cc9f8c5b47aa0516776ae4fa47a9337c291d5d
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2023-01-20 18:55:35 -0300

    chore: add more examples on OOBasics file

--- src/main/scala/essentials/part2oop/OOBasics.scala
+++ src/main/scala/essentials/part2oop/OOBasics.scala
@@ -1,26 +1,58 @@
 package essentials.part2oop
 
-object OOBasics extends App {
-  // classes
-  class Person(val name: String, age: Int): // constructor signature
-    // fields
-    val allCaps = name.toUpperCase
-    // methods
-    def greet(name: String): Unit =
-      println(s"${this.name} says: Hi $name")
+import java.time.LocalDate
 
-    def greet(): String =
-      s"Hi, everyone, my name is $name"
+// classes
+class Person(val name: String, age: Int): // constructor signature
+  // aux constructor
+  def this(name: String) = this(name, 0)
+  // fields
+  val allCaps = name.toUpperCase
+  // methods
+  def greet(name: String): Unit =
+    println(s"${this.name} says: Hi $name")
+  def greet(): String =
+    s"Hi, everyone, my name is $name"
 
-    // aux constructor
-    def this(name: String) = this(name, 0)
+class Writer(firstName: String, lastName: String, val yearOfBirth: Int):
+  def fullName: String = s"$firstName $lastName"
 
+class Novel(title: String, yearOfRelease: Int, author: Writer):
+  def authorAge: Int = yearOfRelease - author.yearOfBirth
+  // FIXME: create a equals and hash methods on Writer
+  def isWrittenBy(author: Writer): Boolean = this.author == author
+  def copy(newYear: Int): Novel = new Novel(title, newYear, author)
+
+// Immutable counter
+class Counter(count: Int = 0):
+  def increment: Counter = increment(1)
+  def increment(n: Int): Counter = new Counter(count + n)
+  def decrement: Counter = decrement(1)
+  def decrement(n: Int): Counter = increment(-n)
+  def print: Unit = println(count)
+
+object OOBasics extends App:
+  println("== PERSON ==")
   val aPerson = new Person("John", 26)
-
   println(aPerson.name)
   println(aPerson.allCaps)
   // println(aPerson.age) // age is not a field, only a constructor parameter
   println(aPerson.greet("pedro"))
   println(aPerson.greet())
 
-}
+  println("== NOVEL + WRITER ==")
+  val author = new Writer("Joao", "Maria", 1983)
+  val authorFaker = new Writer("JoAo", "Maria", 1983)
+  println(author.fullName)
+  val novel = new Novel("My book", 1990, author)
+  println(novel.isWrittenBy(author))
+  println(novel.isWrittenBy(authorFaker))
+  println(novel.authorAge)
+  println(novel.copy(2000).authorAge)
+
+  println("== COUNTER ==")
+  val c = new Counter(10)
+  c.increment.print
+  c.decrement.print
+  c.decrement(5).print
+  c.increment(5).print
